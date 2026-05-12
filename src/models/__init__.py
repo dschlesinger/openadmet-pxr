88e107f26f6f04b65faeb894fce7d072ec4d54d2
dschlesinger <denalischlesinger06@gmail.com>
2026-05-11 21:07:47 -0400

    Add tabpfn model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -11,6 +11,7 @@ from models.knn import KNN
 from models.linear_regression import LinearRegression
 from models.mlp import MLP
 from models.symbolic_regression import SymbolicRegression
+from models.tabpfn_model import TabPFN
 from models.xgboost_model import XGBoost
 
 REGISTRY: dict[str, type[PXRModel]] = {
@@ -21,6 +22,7 @@ REGISTRY: dict[str, type[PXRModel]] = {
     LinearRegression.name: LinearRegression,
     MLP.name: MLP,
     SymbolicRegression.name: SymbolicRegression,
+    TabPFN.name: TabPFN,
     XGBoost.name: XGBoost,
 }
 
